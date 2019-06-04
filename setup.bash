00a3935149f7a6e018cb1a070966e30d9d472ac3
NaokiTakahashi <>
2019-06-05 02:32:20 +0900

    Fix vcs not found

--- setup.bash
+++ setup.bash
@@ -1,19 +1,21 @@
 #!/bin/bash
-sudo apt update
+sudo apt update && \
 sudo apt install -y \
   curl \
   gnupg2 \
-  lsb-release
-curl http://repo.ros2.org/repos.key | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64,arm64] http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list'
-sudo apt update
+  lsb-release \
+  && \
+curl http://repo.ros2.org/repos.key | sudo apt-key add - && \
+sudo sh -c 'echo "deb [arch=amd64,arm64] http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list' && \
+sudo apt update && \
 sudo apt install -y \
   build-essential \
   make \
   cmake \
   git \
   python3-pip \
-  wget
+  wget \
+  && \
 python3 -m pip install -U \
   rosdep \
   catkin-pkg \
@@ -37,25 +39,26 @@ python3 -m pip install -U \
   pytest \
   pytest-cov \
   pytest-runner \
-  setuptools
-echo "export PATH=$PATH:~/.local/bin" >> ~/.bashrc
-source ~/.bashrc
+  setuptools \
+  && \
 sudo apt install --no-install-recommends -y \
   libasio-dev \
-  libtinyxml2-dev
-mkdir -p ros2_ws/src
-cd ros2_ws/
-wget https://raw.githubusercontent.com/ros2/ros2/release-latest/ros2.repos
-vcs import src < ros2.repos
-sudo rosdep init
-rosdep update
-rosdep install \
-  --from-paths src \
-  --ignore-src \
-  --rosdistro dashing -y \
-  --skip-keys "console_bridge fastcdr fastrtps libopensplice67 libopensplice69 rti-connext-dds-5.3.1 urdfdom_headers"
-sudo apt install -y libopensplice69
-sudo apt install -q -y rti-connext-dds-5.3.1
+  libtinyxml2-dev \
+  && \
+mkdir src && \
+wget https://raw.githubusercontent.com/ros2/ros2/release-latest/ros2.repos && \
+bash -c 'export PATH=$PATH:~/.local/bin; \
+  vcs import src < ros2.repos; \
+  sudo rosdep init; \
+  rosdep update; \
+  rosdep install \
+    --from-paths src \
+	--ignore-src \
+  	--rosdistro dashing -y \
+  	--skip-keys "console_bridge fastcdr fastrtps libopensplice67 libopensplice69 rti-connext-dds-5.3.1 urdfdom_headers"' && \
+sudo apt install -y libopensplice69 && \
+sudo apt install -q -y rti-connext-dds-5.3.1 && \
 cd /opt/rti.com/rti_connext_dds-5.3.1/resource/scripts && \
-source ./rtisetenv_x64Linux3gcc5.4.0.bash; cd -
-colcon build --symlink-install --merge-install
+source ./rtisetenv_x64Linux3gcc5.4.0.bash; cd - && \
+bash -c 'export PATH=$PATH:~/.local/bin; colcon build --symlink-install --merge-install' && \
+echo "export PATH=$PATH:~/.local/bin" >> ~/.bashrc
